6a26cdda6ef2b6e09d17ef1a1d30e8c21e25ff18
javarouka(이항희) <javarouka@coupang.com>
2015-05-25 13:07:14 +0900

    add spooky  - fix param

--- src/server/service/SiteNavigation.js
+++ src/server/service/SiteNavigation.js
@@ -1,4 +1,5 @@
 var express = require("express"),
+    fs = require("fs"),
     logger = require("./../logger"),
     site = require("./modules/Site"),
     router = express.Router();
@@ -28,8 +29,20 @@ router.post("/capture", function(req, res, next) {
     }
     else {
         site.capture(req.body.url, './image.png', req.body.selector)
-            .then(function (result) {
-                res.send("<img src='" + result + "'>");
+            .then(function (attachment) {
+                console.log(">>>>>", attachment);
+                fs.readFile(attachment, {encoding: 'base64'}, function(err, base64data) {
+                    if( err ) {
+                        res.send({
+                            "success": false,
+                            "encode": ""
+                        })
+                    }
+                    res.send({
+                        "success": true,
+                        "encode": base64data
+                    });
+                });
             })
             .catch(function (err) {
                 res.send(err);
